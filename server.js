3dd87e20cc2ab616d239f2945eda53b5d12c7b23
Bipin Bhandari <bhandaribipin3.39@gmail.com>
2022-11-06 01:55:03 +0545

    dlk

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5000", "https://bipinbhandari.onrender.com"],
+    origin: ["http://localhost:5000", "https://bipinbhandari.onrender.com/"],
   })
 );
 
